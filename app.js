2d2adc0736f0cf271ed89ab7bad5df0fc18c4bd3
RoAndKi <waxhyze@gmail.com>
2026-03-07 02:59:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = 'https://mpcdpmljpottifmshqht.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wY2RwbWxqcG90dGlmbXNocWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MzUwMTYsImV4cCI6MjA4ODQxMTAxNn0.j40fTgu3OqCRVS2A_EBZNyc5RxtsCBgCwnGZoeWenQY';
+
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const chatBox = document.getElementById('chat-box');
